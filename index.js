5957721eae9d18f8d08da2576d8cf4c063cdffb7
bgaffney314 <bgaffney314@gmail.com>
2015-05-13 22:35:14 -0700

    Exported more middleware functions

--- index.js
+++ index.js
@@ -2,8 +2,9 @@ module.exports = function FilterModule(options){
     return {
         modifiers  : require('./lib/modifiers')(options),
         filters    : require('./lib/filters')(options),
-        mappers    : require('./lib/mappers')(options),
+        validators : require('./lib/validators')(options),
         selectors  : require('./lib/selectors')(options),
-        converters : require('./lib/mappers')(options)
+        mappers    : require('./lib/mappers')(options),
+        converters : require('./lib/converters')(options)
     }
 };
